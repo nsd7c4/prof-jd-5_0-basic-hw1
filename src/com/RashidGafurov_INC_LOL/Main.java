ff6e61e4883f2053b0bc0984ed919323a9fe29d1
rashidgafurov <nsd7c4@gmail.com>
2022-03-01 12:49:11 -0500

    Initial commit First Home work

---
+++ src/com/RashidGafurov_INC_LOL/Main.java
@@ -0,0 +1,9 @@
+package com.RashidGafurov_INC_LOL;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        // First Homework prof-jd-5_0-basic-hw1 by Rashid Gafurov for Skypro. TY
+    }
+}
