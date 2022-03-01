1d076a0cba06a99ee185c0c9024093364ce6c90e
rashidgafurov <nsd7c4@gmail.com>
2022-03-01 13:07:04 -0500

    First Home work Branch 2

--- src/com/RashidGafurov_INC_LOL/Main.java
+++ src/com/RashidGafurov_INC_LOL/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         // First Homework prof-jd-5_0-basic-hw1 by Rashid Gafurov for Skypro. TY
+        // First Homework prof-jd-5_0-basic-hw1 by Rashid Gafurov for Skypro. TY
     }
 }
